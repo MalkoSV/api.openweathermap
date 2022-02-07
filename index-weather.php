0fb5426c170a34392e4a03ae8905254ffad56223
MalkoSV <malsv@ukr.net>
2022-02-07 17:04:27 +0200

    Init commit

---
+++ index-weather.php
@@ -0,0 +1,25 @@
+<?php
+//http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=b7baa67d8544edb9f9160b7300a709be
+
+$url = 'http://api.openweathermap.org/data/2.5/weather';
+
+$options = array(
+    'id' => 700568,
+    'APPID' => 'b7baa67d8544edb9f9160b7300a709be',
+    'units' => 'metric',
+    'lang' => 'en'
+);
+
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_URL, $url.'?'.http_build_query($options));
+
+$response = curl_exec($ch);
+$outputData = json_decode($response, 1);
+curl_close($ch);
+
+echo '<pre>';
+
+print_r($outputData);
+
+
---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+//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   - github
+//https://api.github.com/search/users?q=tomas+repos:>42+followers:>1000
+
+$url = 'http://api.openweathermap.org/data/2.5/weather';
+
+$options = array(
+    'id' => 700568,
+    'APPID' => 'b7baa67d8544edb9f9160b7300a709be',
+    'units' => 'metric',
+    'lang' => 'en'
+);
+
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_URL, $url.'?'.http_build_query($options));
+
+$response = curl_exec($ch);
+$outputData = json_decode($response, 1);
+curl_close($ch);
+
+echo '<pre>';
+
+print_r($outputData);
+
+
